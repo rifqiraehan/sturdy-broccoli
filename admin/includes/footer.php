aacc6d587cc16419ba41b1d135b7ea41b465ee43
rifqiraehan <rifqiraehan86@gmail.com>
2024-05-21 18:50:07 +0700

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+     <strong>Copyright &copy; 2020 <a href="https://www.instagram.com/putrichya_a">Saturn Store</a></strong>
+</footer>
