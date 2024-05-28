bbd3d10019f4e676f3b4442b5f857f1781bd5e8e
rifqiraehan <rifqiraehan86@gmail.com>
2024-05-28 19:10:53 +0700

    Fixed Config

--- includes/conn.php
+++ includes/conn.php
@@ -1,13 +1,13 @@
 <?php
 
 Class Database{
- 
+
 	private $server = "mysql:host=localhost;dbname=ecomm";
 	private $username = "root";
-	private $password = "";
+	private $password = "admin";
 	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
 	protected $conn;
- 	
+
 	public function open(){
  		try{
  			$this->conn = new PDO($this->server, $this->username, $this->password, $this->options);
@@ -16,15 +16,14 @@ Class Database{
  		catch (PDOException $e){
  			echo "There is some problem in connection: " . $e->getMessage();
  		}
- 
+
     }
- 
+
 	public function close(){
    		$this->conn = null;
  	}
- 
 }
 
 $pdo = new Database();
- 
+
 ?>
